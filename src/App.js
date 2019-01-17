c9488dcef6bbb284d18007e175652892dbfe75c6
PUNOLEE <punolee@outlook.com>
2019-01-16 19:20:19 -0600

    closes #10

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Bag from "./components/bag/bag";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import BagIcon from "@material-ui/icons/ShoppingBasket";
+import AccountIcon from "@material-ui/icons/AccountCircle";
 import Popover from "@material-ui/core/Popover";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -12,6 +13,17 @@ import { updateBag } from "./services/actions";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import Badge from "@material-ui/core/Badge";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+import firebase from "./services/firebase.js";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 class App extends Component {
   constructor(props) {
@@ -31,9 +43,15 @@ class App extends Component {
         "default",
         "default"
       ],
-      clicked: []
+      clicked: [],
+      open: false,
+      user: ""
     };
   }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
 
   handleClick = event => {
     this.setState({
@@ -43,7 +61,8 @@ class App extends Component {
 
   handleClose = () => {
     this.setState({
-      anchorEl: null
+      anchorEl: null,
+      open: false
     });
     this.props.updateBag(this.props.items, false);
   };
@@ -52,6 +71,15 @@ class App extends Component {
     import("./products.json").then(json => {
       this.setState({ products: json.products, allproducts: json.products });
     });
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+        // User is signed in.
+      } else {
+        // No user is signed in.
+      }
+    });
+    console.log(firebase.auth().currentUser);
   }
 
   handleFilterClick = (size, index) => event => {
@@ -89,6 +117,35 @@ class App extends Component {
     return num;
   };
 
+  signInNUp = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        //var token = result.credential.accessToken;
+        // The signed-in user info.
+        // ...
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        // ...
+      });
+    this.setState({ open: false });
+  };
+
+  signOut = () => {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      
+      alert("Sign Out Successfully!");
+      
+    }).catch(function(error) {
+      // An error happened.
+    });
+    this.setState({ open: false});
+  };
+
   render() {
     const { anchorEl } = this.state;
     const open = Boolean(anchorEl) || this.props.show;
@@ -110,27 +167,33 @@ class App extends Component {
               );
             })}
           </Paper>
-
-          <div className="App-icon">
-            <IconButton
-              aria-owns={open ? "simple-popper" : undefined}
-              aria-label="bag"
-              onClick={this.handleClick}
-              color="primary"
-            >
-              <Badge badgeContent={this.calculateNumber()} color="secondary">
-                <BagIcon />
-              </Badge>
-            </IconButton>
+          <div>
+            <div className="App-icon">
+              <IconButton
+                aria-owns={open ? "simple-popper" : undefined}
+                aria-label="bag"
+                onClick={this.handleClick}
+                color="primary"
+              >
+                <Badge badgeContent={this.calculateNumber()} color="secondary">
+                  <BagIcon />
+                </Badge>
+              </IconButton>
+            </div>
+            <div className="App-icon">
+              <IconButton color="primary" onClick={this.handleClickOpen}>
+                <AccountIcon />
+              </IconButton>
+            </div>
           </div>
         </div>
         <div className="App-counter">
           {this.state.products.length} Product(s) found.
         </div>
         <Grid container spacing={24}>
-          {this.state.products.map((product,index)=> (
+          {this.state.products.map((product, index) => (
             <Grid item xs={3} key={index}>
-              <ViewCard product={product} key={product.id}/>
+              <ViewCard product={product} key={product.id} />
             </Grid>
           ))}
         </Grid>
@@ -150,6 +213,42 @@ class App extends Component {
         >
           <Bag />
         </Popover>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="draggable-dialog-title"
+        >
+          <DialogTitle id="draggable-dialog-title">User</DialogTitle>
+          <DialogContent>
+            {firebase.auth().currentUser != null ? (
+              <div className="User-info">
+                <Avatar
+                  alt={firebase.auth().currentUser.displayName}
+                  src={firebase.auth().currentUser.photoURL}
+                />
+                <Typography component={"span"} className="User-info-name">
+                  {firebase.auth().currentUser.displayName}
+                </Typography>
+              </div>
+            ) : (
+              <DialogContentText />
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            {firebase.auth().currentUser == null ? (
+              <Button onClick={this.signInNUp} color="primary">
+                Sign In
+              </Button>
+            ) : (
+              <Button onClick={this.signOut} color="primary">
+                Sign Out
+              </Button>
+            )}
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyCHC-CkbY_fD1ItsPlNvfFUo8qarSxTqTs",
+  authDomain: "shopping-cart-ffa93.firebaseapp.com",
+  databaseURL: "https://shopping-cart-ffa93.firebaseio.com",
+  projectId: "shopping-cart-ffa93",
+  storageBucket: "shopping-cart-ffa93.appspot.com",
+  messagingSenderId: "574085777579"
+};
+firebase.initializeApp(config);
+export default firebase;
